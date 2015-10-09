9145384ad826124d3e54a127d8d77df585696945
sykopompos <sykopompos@gmail.com>
2015-10-09 16:09:41 -0400

    mako: force dex2oat not to use swap file
    adapted from flo https://android.googlesource.com/device/asus/flo/+/7dbfea4f75dc4cb8954479bcfe6975abcfd2aa25%5E!/#F0

--- device.mk
+++ device.mk
@@ -282,6 +282,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
+	
+PRODUCT_PROPERTY_OVERRIDES += \
+        dalvik.vm.dex2oat-swap=false
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
